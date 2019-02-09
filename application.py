7d2e1234a0660ce4c6e5f070c7b4e53c2a1a55ae
root <root@localhost.localdomain>
2019-02-09 21:36:53 +0530

    version1

--- application.py
+++ application.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, session
+from flask_session import Session
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 app = Flask(__name__)
 
@@ -7,10 +16,38 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/register")
-def register():
-    return render_template("register.html")
-
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
-    return render_template("login.html")
+    name = request.form.get("username")
+    return render_template("book.html", username=name)
+
+@app.route("/createaccount")
+def createaccount():
+    return render_template("createaccount.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("username")
+    return render_template("index.html", name=name)
+
+
+
+# after login main (book) section start here
+
+@app.route("/givereview", methods=["POST"])
+def givereview():
+    return render_template("givereview.html")
+
+@app.route("/savereview", methods=["POST"])
+def savereview():
+    return render_template("viewreview.html")
+
+@app.route("/viewreview")
+def viewreview():
+    return render_template("viewreview.html")
+
+
+
+
+
+
