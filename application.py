fa77f3a8bfd08bccc8d36950eea8d5099b0e5c6b
root <root@localhost.localdomain>
2019-02-18 23:45:32 +0530

    final submission 17 feb

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
+
 engine = create_engine('postgres://ntcwaduvpgeowm:b5459817675574612c7cf55ba6ffbb0752457b10239c15c7084ef858b9277349@ec2-54-83-17-151.compute-1.amazonaws.com:5432/dce25fb79tcjcv', echo = True)
 db = scoped_session(sessionmaker(bind=engine))
 
@@ -13,40 +14,46 @@ db = scoped_session(sessionmaker(bind=engine))
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-app = Flask(__name__)
+username = None
 
 @app.route("/")
 def index():
-       return render_template("index.html",error="Login to give book review")
+       return render_template("index.html",error="Login to give book review",username="Not Login")
 
 
 # after login main (book) section start here
 
 @app.route("/givereview/<int:book_id>")
 def givereview(book_id):  
+       username = session.get('username')
        bookselect = db.execute("SELECT * FROM book WHERE book_id = :book_id", {"book_id": book_id}).fetchone()
-       return render_template("givereview.html", book=bookselect, error="this is your selected book give review")
+       return render_template("givereview.html", book=bookselect, error="this is your selected book give review",username=username)
 
 
 
-@app.route("/savereview", methods=["POST"])
-def savereview():
+@app.route("/savereview/<int:book_id>", methods=["POST"])
+def savereview(book_id):
        if request.method == 'POST':
               scale = request.form.get('star')
               texts = request.form.get('texts')
-              return "save review"
-              '''
-              student_id = db.execute("SELECT student_id FROM student WHERE username = :username", {"username": username}).fetchone();
-              book_id = db.execute("SELECT book_id FROM book WHERE book_id = :book_id", {"book_id": book_id}).fetchone();
+              username = session.get('username')
+
+              sid = db.execute("SELECT student_id FROM student WHERE username = :username", {"username": username}).fetchone();
+              student_id = sid[0]
+              bid = db.execute("SELECT book_id FROM book WHERE book_id = :book_id", {"book_id": book_id}).fetchone();
+              book_id = bid[0]
               db.execute("INSERT INTO review (scale, texts, student, book) VALUES (:scale, :texts, :student, :book)",
                     {"scale": scale, "texts": texts, "student": student_id, "book": book_id})
-              '''
+              db.commit()
+              return "review saved succesfull"
+              
 
 
 @app.route("/viewreview")
 def viewreview():
-       return render_template("viewreview.html")
+       username = session.get('username')
+       usrreview = db.execute("SELECT * FROM review").fetchall();
+       return render_template("viewreview.html", review=usrreview, error="Your total review list", username=username)
 
 
 
@@ -64,17 +71,18 @@ def login():
                      raw_password = db.execute("SELECT passwrd FROM student WHERE username = :username", {"username": username}).fetchone()
                      password_db = raw_password[0]
                      if(password == password_db):
+                            session['username'] = username
                             booksdb = db.execute("SELECT * FROM book ").fetchall()
                             return render_template("book.html", books=booksdb, error="login succesfull, select book to give review", username=username)
                      else:
-                            return render_template("index.html",error="Try login again, password not match")
+                            return render_template("index.html",error="Try login again, password not match",username="Not Login")
        else:
-              return render_template("index.html",error="something went wrong, try again")
+              return render_template("index.html",error="something went wrong, try again",username="Not Login")
 
 
 @app.route("/createaccount")
 def createaccount():
-       return render_template("createaccount.html",error="welcome new user")
+       return render_template("createaccount.html",error="welcome new user",username="Not Login")
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -97,6 +105,6 @@ def register():
 
 
 if __name__ == '__main__':
-       app.run()
+    app.run(debug=True)
 
 
